71f596194a678adf167d5ecf71a3465c19b7a382
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2021-07-14 16:40:22 -0300

    maintenance script

---
+++ scripts/cron/maintenance.sh
@@ -0,0 +1,16 @@
+log() { echo "[$(date)] $1" ; }
+
+log "Starting new maintenance job"
+
+# Deleting log files with size 0
+for drive_keep_alive_log_file in $(ls ~/chia/logs/cron/drive_keep_alive/drive_keep_alive_*.log); do
+  filesize=$(stat -c%s "${drive_keep_alive_log_file}")
+  if ((filesize == 0)); then
+    echo "Deleting file ${drive_keep_alive_log_file} with size ${filesize}"
+    rm ${drive_keep_alive_log_file}
+  fi
+done
+
+echo
+echo "--------------------------------------------------------------------------"
+echo
--- scripts/utils/add_folders_plot.sh
+++ scripts/utils/add_folders_plot.sh
@@ -1,10 +1,20 @@
-cd ~/chia/chia-blockchain
-. ./activate
+. ~/chia/chia-blockchain/activate
 chia init
 chia start farmer
 
+echo
+echo "---------------------------------------"
+echo
+
 for dir in /media/cripto-hilkner/*; do
+#for dir in /media/chia-storage/*; do
   dir_name=$(echo "${dir}/" | sed 's/ /\\ /g')
   echo "Adding dir ${dir_name}" 
   chia plots add -d "${dir_name}"
 done
+
+echo
+echo "---------------------------------------"
+echo
+
+chia plots show
