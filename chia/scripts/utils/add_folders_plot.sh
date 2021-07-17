2cf8c19734db778804a1db42dd4468a18755f393
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2021-07-17 05:15:21 -0300

    add flax plots to farm as well

--- chia/scripts/utils/add_folders_plot.sh
+++ chia/scripts/utils/add_folders_plot.sh
@@ -1,3 +1,5 @@
+log "---------- CHIA ----------"
+
 . ~/chia/chia-blockchain/activate
 chia init
 chia start farmer
@@ -18,3 +20,35 @@ echo "---------------------------------------"
 echo
 
 chia plots show
+deactivate
+
+echo
+echo "---------------------------------------"
+echo
+
+log "---------- FLAX ----------"
+
+. ~/chia/chia-blockchain/activate
+flax init
+flax start farmer
+
+echo
+echo "---------------------------------------"
+echo
+
+for dir in /media/cripto-hilkner/*; do
+#for dir in /media/chia-storage/*; do
+  dir_name=$(echo "${dir}/" | sed 's/ /\\ /g')
+  echo "Adding dir ${dir_name}" 
+  flax plots add -d "${dir_name}"
+done
+
+echo
+echo "---------------------------------------"
+echo
+
+flax plots show
+
+echo
+echo "---------------------------------------"
+echo
