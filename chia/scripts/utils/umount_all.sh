ce14ba7ef6dd7b55d6b3c3180281553a5d95479f
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2021-07-19 15:45:05 -0300

    fixes

--- chia/scripts/utils/umount_all.sh
+++ chia/scripts/utils/umount_all.sh
@@ -2,5 +2,5 @@ log() { echo "[$(date)] $1" ; }
 
 for dir in $(df | grep "/media" | awk '{print $1}'); do
   log "Umount'ing ${dir}"
-  umount ${dir}
+  cat ~/chia/scripts/utils/get_pw.txt | sudo -S umount ${dir}
 done
