d220ef9a93ae687bf09641162b1e87ae85507c4d
Rodrigo <rodrigo.hilkner@gmail.com>
2021-11-10 22:32:15 -0300

    transform count_og_plots.sh in one-liner

--- chia/scripts/utils/count_og_plots.sh
+++ chia/scripts/utils/count_og_plots.sh
@@ -1,10 +1,1 @@
-cd /media/cripto-hilkner
-hd_folders=( $(ls) )
-for folder in ${hd_folders[@]}; do
-    og_plots=( $(ls ${folder} | grep 'plot-k32-2021-07-0') )
-    og_plots+=( $(ls ${folder} | grep 'plot-k32-2021-06') )
-    og_plots+=( $(ls ${folder} | grep 'plot-k32-2021-05') )
-    if (( ! ${#og_plots[@]} )); then
-        echo $folder: ${#og_plots[@]}
-    fi
-done | sort
+cd /media/cripto-hilkner; hd_folders=( $(ls) ); for folder in ${hd_folders[@]}; do og_plots=( $(ls ${folder} | grep 'plot-k32-2021-07-0') ); og_plots+=( $(ls ${folder} | grep 'plot-k32-2021-06') ); og_plots+=( $(ls ${folder} | grep 'plot-k32-2021-05') ); echo $folder: ${#og_plots[@]}; done | sort
